1dc4c43b51165db2a7bdda2472702276db4a7b81
Raunaq <raunaq.soni@gmail.com>
2019-01-05 23:52:18 +0530

    Added code to detect which player scored the goal

--- Assets/GoalPostCollisionManager.cs
+++ Assets/GoalPostCollisionManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class GoalPostCollisionManager : MonoBehaviour {
 
+    public GameObject BallCollision;
+     BallCollisionChecker ballCollisionChecker;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        ballCollisionChecker = BallCollision.GetComponent<BallCollisionChecker>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,7 +23,8 @@ public class GoalPostCollisionManager : MonoBehaviour {
     {
         if(other.name == "Ball")
         {
-            Debug.Log("Goal!");
+            string LastTouchPlayer = ballCollisionChecker.LastTouchPlayer;
+            Debug.Log("Goal by " + LastTouchPlayer);
 
         }
     }
