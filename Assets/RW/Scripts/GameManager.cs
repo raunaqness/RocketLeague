d5e2cc86a499cf248ab1e15bf3ae5cd988fbe073
Raunaq <raunaq.soni@gmail.com>
2019-01-26 21:20:52 +0530

    StarterProject - FINAL - Scripts Cleaned

---
+++ Assets/RW/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Realtime;
+
+namespace Photon.Pun.Demo.PunBasics
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        static public GameManager Instance;
+        private GameObject instance;
+
+        public GameObject WinnerUI;
+
+        [Tooltip("The prefab to use for representing the player")]
+        [SerializeField]
+        private GameObject playerPrefab;
+        private GameObject ball;
+
+        public GameObject Player1SpawnPosition;
+        public GameObject Player2SpawnPosition;
+
+        public GameObject BallSpawnTransform;
+
+        private GameObject player1, player2;
+
+        // Start Method
+
+        // Update Method
+
+        // Photon Methods
+
+        //Helper Methods
+
+    }
+}
---
+++ Assets/RW/Scripts/Launcher.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Photon.Pun.Demo.PunBasics
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+    
+        [Tooltip("The UI Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+
+        [Tooltip("The Ui Text to inform the user about the connection progress")]
+        [SerializeField]
+        private Text feedbackText;
+
+        [Tooltip("The maximum number of players per room")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 2;
+
+        bool isConnecting;
+
+        string gameVersion = "1";
+
+        [Space(10)]
+        [Header("Custom Variables")]
+        public InputField PlayerName;
+        public InputField RoomName;
+
+        [Space(5)]
+        public Text PlayerStatus;
+        public Text ConnectionStatus;
+
+        [Space(5)]
+        public GameObject RoomJoinUI;
+        public GameObject Button_LoadArena;
+        public GameObject Button_JoinRoom;
+
+        string _PlayerName = "";
+        string _RoomName = "";
+
+        // Helper Methods
+
+
+        // Tutorial Methods
+
+
+        // Photon Methods
+
+    } 
+}
